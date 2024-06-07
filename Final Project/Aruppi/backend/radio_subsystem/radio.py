62ef79729dcb52fb83daf2885a2448d3655f13bd
Cach2027 <cacelish@udistrital.edu.co>
2024-06-07 14:35:26 -0500

    Hi its me

--- Final Project/Aruppi/backend/radio_subsystem/radio.py
+++ Final Project/Aruppi/backend/radio_subsystem/radio.py
@@ -1,6 +1,17 @@
 """
-This is a main file of Aruppi project (initial point).
+This file has some classes related to radio.
 Authors:
 -> Xiomara Salome Arias Arias < xsariasa@udistrital.edu.co >
 -> Carlos Andres Celis Herrera < cacelish@udistrital.edu.co >
 """
+
+class Radio:
+    """This class represent the context of radio state"""
+
+    def __init__(self, station:str) -> None:
+        self.station=station
+        self._state=Play(self)
+
+    def ChangeState(self,state):
+        """This method define the changes of state"""
+        self.state=state

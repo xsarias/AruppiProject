2abd0cf7c6849d5de18faa34c3cabc1281dab55b
Cach2027 <cacelish@udistrital.edu.co>
2024-06-08 15:31:39 -0500

    update news

--- Final Project/Aruppi/backend/radio_subsystem/radio_facade.py
+++ Final Project/Aruppi/backend/radio_subsystem/radio_facade.py
@@ -26,15 +26,4 @@ class RadioFacade:
     def previous(self):
         """This method sets the state to Previous and performs the action"""
         self.radio.previous()
-
-# Ejemplo de uso
-if __name__ == "__main__":
-    radio_facade = RadioFacade("Station 1")
-    print("start facade")
-    radio_facade.play()     # La estación ya está en estado Play
-    radio_facade.pause()    # Cambia el estado a Pause
-    radio_facade.next()     # Cambia al siguiente estación y establece el estado a Play
-    radio_facade.previous() # Cambia al estación anterior y establece el estado a Play
-    radio_facade.pause()    # Cambia el estado a Pause
-    radio_facade.play()     # Cambia el estado a Play
-    print("end facade")
+    

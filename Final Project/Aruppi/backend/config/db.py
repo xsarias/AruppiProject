992105f63f185597ab6490ec4bf7ffa0626b4fd7
Cach2027 <cacelish@udistrital.edu.co>
2024-06-11 07:04:51 -0500

    update main

--- Final Project/Aruppi/backend/config/db.py
+++ Final Project/Aruppi/backend/config/db.py
@@ -5,61 +5,18 @@ Authors:
 -> Carlos Andres Celis Herrera < cacelish@udistrital.edu.co >
 """
 
+from sqlalchemy import create_engine, MetaData
 import os
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
-load_dotenv()
-engine = create_engine(
-    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_URL')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-)
-Session = sessionmaker(bind=engine)
-session = Session()
-metadata = MetaData()
-series = Table ("series",
-                metadata,
-                Column("anime_id", Integer(10), primary_key=True, autoincrement=True),
-                Column("title", String(50)),
-                Column("description", String(100)),
-                Column("category", String(50)),
-                Column("anime_type", String(50)),
-                Column("producer", String(50)),
-                Column("episodes_amount", Integer(10))
-             )
-movies = Table ("movies",
-                metadata,
-                Column("anime_id", Integer(10), primary_key=True, autoincrement=True),
-                Column("title", String(50)),
-                Column("description", String(50*2)),
-                Column("category", String(50)),
-                Column("anime_type", String(50)),
-                Column("producer", String(50)),
-                Column("running_time", Float(10))
-                )
-ovas = Table("ovas",
-                metadata,
-                Column("anime_id", Integer(10), primary_key=True, autoincrement=True),
-                Column("title", String(50)),
-                Column("description", String(100)),
-                Column("category", String(50)),
-                Column("anime_type", String(50)),
-                Column("producer", String(50)),
-                Column("episodes_amount", Integer(10))
-            )
-user = Table("user",
-                metadata,
-                Column("username", String(50), primary_key=True),
-                Column("password", String(50)),
-                Column("grants", String(50))
-            )
-station = Table("station",
-                metadata,
-                Column("station", String, primary_key=True),
-                )
-news = Table("news",
-                metadata,
-                Column("title", String, primary_key=True),
-                Column("info", String)
-            )
-metadata.create_all(engine)
+
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_ROOT_PASSWORD = os.getenv("MYSQL_ROOT_PASSWORD")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+
+URL_CONNECTION = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_ROOT_PASSWORD}@{MYSQL_HOST}:3306/{MYSQL_DATABASE}"
+
+meta = MetaData()
+
+engine = create_engine(URL_CONNECTION)
+conn = engine.connect()
